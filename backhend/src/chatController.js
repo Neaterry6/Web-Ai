8f9cdf11b12a7dae31353799e4b059c2f5bfecef
Akewushola abdulbakri temitope <neaterry6@gmail.com>
2025-04-21 01:21:07 +0100

    Update chatController.js

--- backhend/src/chatController.js
+++ backhend/src/chatController.js
@@ -1,1 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
 
+router.post("/respond", async (req, res) => {
+  const { message } = req.body;
+
+  if (message.toLowerCase().includes("time")) {
+    const apiResponse = await axios.get("https://ap-c4yl.onrender.com/api/datetime");
+    return res.send({ reply: `Current time is: ${apiResponse.data.time}` });
+  }
+
+  res.send({ reply: "Ayanfe is processing your query..." });
+});
+
+module.exports = router;
