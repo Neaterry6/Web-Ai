c26ed8b445932dacbbde7263ae73c5bca6948e9d
Akewushola abdulbakri temitope <neaterry6@gmail.com>
2025-04-21 01:19:48 +0100

    Update server.js

--- backhend/src/server.js
+++ backhend/src/server.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
+const authRoutes = require("./auth");
+const chatRoutes = require("./chatController");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use("/auth", authRoutes);
+app.use("/chat", chatRoutes);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
