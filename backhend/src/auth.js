669bdc92a184bcc4a63e9d30106b32a54c475b41
Akewushola abdulbakri temitope <neaterry6@gmail.com>
2025-04-21 01:20:26 +0100

    Update auth.js

--- backhend/src/auth.js
+++ backhend/src/auth.js
@@ -1,1 +1,29 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("./models/userModel");
 
+const router = express.Router();
+
+// Signup
+router.post("/signup", async (req, res) => {
+  const { email, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({ email, password: hashedPassword });
+  await newUser.save();
+  res.send({ message: "User registered!" });
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && await bcrypt.compare(password, user.password)) {
+    const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    res.send({ token });
+  } else {
+    res.status(401).send({ error: "Invalid credentials" });
+  }
+});
+
+module.exports = router;
